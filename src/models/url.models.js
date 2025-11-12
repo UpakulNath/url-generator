3ef493e70149c592d8c257e007daa58dca33de23
Upakul <upakuln1@gmail.com>
2025-11-12 20:07:46 +0530

    add pre-hooks to the models

--- src/models/url.models.js
+++ src/models/url.models.js
@@ -1,19 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import Hashids from "hashids";
 
-const urlSchema = new Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId, // ObjectId type
-    ref: "User", // Reference to the User model
-    required: true,
+const urlSchema = new Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId, // ObjectId type
+      ref: "User", // Reference to the User model
+      required: true,
+    },
+    long_url: {
+      type: String,
+      required: true,
+    },
+    short_code: {
+      type: String,
+      unique: true,
+    },
   },
-  long_url: {
-    type: String,
-    required: true,
-  },
-  short_code: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  timestamps: true,
+  { timestamps: true },
+);
+
+const hashids = new Hashids(process.env.SECRET_SALT, 6);
+
+urlSchema.pre("save", function (next) {
+  if (!this.short_code) {
+    const shortCode = hashids.encodeHex(this._id.toString());
+    this.short_code = shortCode;
+  }
+  next();
 });
+
+export const Url = mongoose.model("Url", urlSchema);
