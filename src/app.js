48f08eaecb5530b8579ecd1a986a24036ba563f1
Upakul <upakuln1@gmail.com>
2025-12-26 19:31:40 +0530

    change permission settings for cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ app.use(express.urlencoded({ extended: true, limit: "16 kb" }));
 
 app.use(
   cors({
-    origin: [`http://localhost:${port}`,`http://127.0.0.1:5500`],
+    origin: [
+      `http://localhost:${port}`,
+      `http://127.0.0.1:5500`,
+      `https://miniurlapp.netlify.app`,
+    ],
     methods: ["GET", "POST"],
     credentials: true, //allow cookies/auth headers
     allowedHeaders: ["Content-type", "Authorization"],
